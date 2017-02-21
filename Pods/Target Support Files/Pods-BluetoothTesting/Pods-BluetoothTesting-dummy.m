e23c7bbb569bd32cbdfb960eaa90ecaa0134085a
Arianna Moreno <ariannamoreno@Ariannas-MacBook-Pro.local>
2017-02-21 14:12:23 -0700

    Updated the string that matches the bluetooth device with your device from 'yourBean' to 'Bean+'

---
+++ Pods/Target Support Files/Pods-BluetoothTesting/Pods-BluetoothTesting-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BluetoothTesting : NSObject
+@end
+@implementation PodsDummy_Pods_BluetoothTesting
+@end
---
+++ Pods/Target Support Files/Pods-BluetoothTestingUITests/Pods-BluetoothTestingUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BluetoothTestingUITests : NSObject
+@end
+@implementation PodsDummy_Pods_BluetoothTestingUITests
+@end
