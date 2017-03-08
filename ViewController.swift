89e60d5a739e5b2738bc919cfee6bba2a05e2bcc
armoren4 <Arianna.moreno@asu.edu>
2017-03-08 16:59:19 -0700

    First Actual Commit
    Hopefully this will be the info you need to upload this project, EXCEPT for the pods. You'll need to install those on your own. They include:
    AFNetworking, Bean-iOS-OSX-SDK, Bolts, FBSDKCoreKit, MBProgressHUD

---
+++ ViewController.swift
@@ -0,0 +1,108 @@
+//
+//  ViewController.swift
+//  BluetoothTesting
+//
+//  Created by Arianna Moreno on 2/14/17.
+//  Copyright © 2017 Arianna's Apps. All rights reserved.
+//
+
+import UIKit
+import Bean_iOS_OSX_SDK
+
+class ViewController: UIViewController, PTDBeanManagerDelegate, PTDBeanDelegate {
+    
+    // Declare variables we will use throughout the app
+    var beanManager: PTDBeanManager?
+    var yourBean: PTDBean?
+    var lightState: Bool = false
+    
+    // MARK: Properties
+    @IBOutlet weak var ledTextLabel: UILabel!
+
+    // After view is loaded into memory, we create an instance of PTDBeanManager
+    // and assign ourselves as the delegate
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        beanManager = PTDBeanManager()
+        beanManager!.delegate = self
+    }
+    
+    // After the view is added we will start scanning for Bean peripherals
+    override func viewDidAppear(_ animated: Bool) {
+        startScanning()
+    }
+    
+    // Bean SDK: We check to see if Bluetooth is on.
+    func beanManagerDidUpdateState(_ beanManager: PTDBeanManager!) {
+        var scanError: NSError?
+        
+        if beanManager!.state == BeanManagerState.poweredOn {
+            startScanning()
+            if var e = scanError {
+                print(e)
+            } else {
+                print("Please turn on your Bluetooth")
+            }
+        }
+    }
+    
+    // Scan for Beans
+    func startScanning() {
+        var error: NSError?
+        beanManager!.startScanning(forBeans_error: &error)
+        if let e = error {
+            print(e)
+        }
+    }
+    
+    // We connect to a specific Bean
+    func beanManager(_ beanManager: PTDBeanManager!, didDiscover bean: PTDBean!, error: Error!) {
+        if let e = error {
+            print(e)
+        }
+        
+        print("Found a Bean: \(bean.name)")
+        if bean.name == "Bean+" {
+            yourBean = bean
+            print("got your bean")
+            connectToBean(bean: yourBean!)
+        }
+    }
+    
+    // Bean SDK: connects to Bean
+    func connectToBean(bean: PTDBean) {
+        var error: NSError?
+        beanManager?.connect(to: yourBean, withOptions:nil, error: &error)
+        yourBean?.delegate = self    }
+    
+    // Bean SDK: Send serial datat to the Bean
+    func sendSerialData(beanState: NSData) {
+        yourBean?.sendSerialData(beanState as Data!)
+    }
+    
+    // Change LED text when button is pressed
+    func updateLedStatusText(lightState: Bool) {
+        let onOffText = lightState ? "ON" : "OFF"
+        ledTextLabel.text = "Led is: \(onOffText)"
+    }
+    
+    // Mark: Actions
+    // When we pressed the button, we change the light state and
+    // We update date the label, and send the Bean serial data
+    @IBAction func pressMeButtonToToggleLED(_ sender: AnyObject) {
+        lightState = !lightState
+        updateLedStatusText(lightState: lightState)
+        let data = NSData(bytes: &lightState, length: MemoryLayout<Bool>.size)
+        sendSerialData(beanState: data)
+        
+        if ledTextLabel.text == nil {
+            ledTextLabel.text = "Led is: OFF"
+        } else if ledTextLabel.text == "Led is: OFF" {
+            ledTextLabel.text = "Led is: ON"
+        } else {
+            ledTextLabel.text = "Led is: OFF"
+        }
+    }
+
+}
+
